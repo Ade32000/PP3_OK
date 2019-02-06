fa9672d9ffbb23ec480f9fea69e856671ea4a43d
nicolas <nicolas.lasportes@gmail.com>
2019-02-06 08:04:23 +0100

    fonctionnel + spahscreen

--- comVisuelle/platforms/android/app/build/intermediates/assets/debug/www/js/database.js
+++ comVisuelle/platforms/android/app/build/intermediates/assets/debug/www/js/database.js
@@ -18,6 +18,7 @@ function fillDB(tx)
         dataType: "json"
     }).done(function(data)
     {
+        console.log(data)
         allPictures = data;
         db.transaction(fillPictures, errorCB, successCB);
     });
@@ -28,29 +29,30 @@ function fillDB(tx)
         dataType: "json"
     }).done(function(data1)
     {
+        console.log(data1)
         allCategories = data1;
         db.transaction(fillCategories, errorCB, successCB);
     });
-
+    
     $.ajax({
         type: "get",
         url: "json/to_belong.json",
         dataType: "json"
-    }).done(function(data3)
+    }).done(function(data2)
     {
-        console.log(data3);
-        allToBelong = data3;
+        console.log(data2)
+        allToBelong = data2;
         db.transaction(fillToBelong, errorCB, successCB);
-        db.transaction(selectToBelong, errorCB, successCB);
     });
 
     $.ajax({
         type: "get",
         url: "json/tags.json",
         dataType: "json"
-    }).done(function(data4)
+    }).done(function(data3)
     {
-        alltags = data4;
+        console.log(data3)
+        alltags = data3;
         db.transaction(fillTags, errorCB, successCB);
     });
 
@@ -58,16 +60,15 @@ function fillDB(tx)
         type: "get",
         url: "json/to_have.json",
         dataType: "json"
-    }).done(function(data5)
+    }).done(function(data4)
     {
-        allToHave = data5;
-        console.log(allToHave);
+        console.log(data4)
+        allToHave = data4;
         db.transaction(fillToHave, errorCB, successCB);
-        db.transaction(selectToHave, errorCB, successCB);
     });       
 }
 
-function errorCB(tx, err)
+function errorCB(err)
 {
     console.log(err);
 }
@@ -77,30 +78,16 @@ function successCB()
     console.log("success!");
 }
 
-function selectToBelong(tx)
-{
-    tx.executeSql("SELECT * FROM to_belong", [], function(tx, result)
-    {
-        console.log(result.rows);
-    });
-}
-
-function selectToHave(tx)
-{
-    tx.executeSql("SELECT * FROM to_have", [], function(tx, result)
-    {
-        console.log(result.rows);
-    });
-}
-
 //remplie la table pictures avec les données du json
 function fillPictures(tx)
 {
+    tx.executeSql("DROP TABLE IF EXISTS pictures");
     tx.executeSql("CREATE TABLE IF NOT EXISTS pictures (picture_id unique, picture_name, picture_url)");
     for(var i = 0; i < allPictures.length; i++)
-    {pictures
-        var sql = 'INSERT INTO pictures (picture_id, picture_name, picture_url) VALUES (' 
-        + allPictures[i].picture_id + ', "' + allPictures[i].picture_name + '", "' + allPictures[i].picture_url + '")';
+    {
+        console.log("je boucle dans pictures");
+        var sql = 'INSERT INTO pictures VALUES (' + 
+        allPictures[i].picture_id + ', "' + allPictures[i].picture_name + '", "' + allPictures[i].picture_url + '")';
         tx.executeSql(sql);
     }
 }
@@ -108,9 +95,11 @@ function fillPictures(tx)
 //remplie la table categories avec les données du json
 function fillCategories(tx)
 {
+    tx.executeSql("DROP TABLE IF EXISTS categories");
     tx.executeSql("CREATE TABLE IF NOT EXISTS categories (category_id unique, category_name)");
     for(var i = 0; i < allCategories.length; i++)
     {
+        console.log("je boucle dans categories");
         var sql = 'INSERT INTO categories (category_id, category_name) VALUES (' 
         + allCategories[i].category_id + ', "' + allCategories[i].category_name + '")';
         tx.executeSql(sql);
@@ -120,44 +109,25 @@ function fillCategories(tx)
 //remplie la table to_belong avec les données du json
 function fillToBelong(tx)
 {
-    console.log(allToBelong);
-    tx.executeSql("DROP TABLE to_belong IF EXISTS");
+    tx.executeSql("DROP TABLE IF EXISTS to_belong");
     tx.executeSql("CREATE TABLE IF NOT EXISTS to_belong (fk_picture INTEGER, fk_category INTEGER, FOREIGN KEY(fk_picture) REFERENCES pictures(picture_id), FOREIGN KEY(fk_category) REFERENCES categories(category_id))");
     for(var i = 0; i < allToBelong.length; i++)
     {
+        console.log("je boucle dans to_belong");
         var sql = 'INSERT INTO to_belong (fk_category, fk_picture) VALUES (' 
         + allToBelong[i].fk_category + ', "' + allToBelong[i].fk_picture + '")';
         tx.executeSql(sql);
     };
 }
 
-function checkIfTablesExist(tx)
-{
-    tx.executeSql("SELECt * FROM pictures", [], function(tx, result)
-    {
-        console.log("Je select * from picture");
-        console.log(result.rows);
-    });
-}
-
-// function ifDBDoesntExists()
-// {
-//     console.log("creating db");
-//     db.transaction(fillDB, errorCB, executeScan);
-// }
-
-// function ifDBExists()
-// {
-//     console.log("la bdd existe");
-//     db.transaction(destroyDB, errorCB, successCB);
-// }
-
 //remplie la table to_have avec les données du json
 function fillTags(tx)
 {
+    tx.executeSql("DROP TABLE IF EXISTS tags");
     tx.executeSql("CREATE TABLE IF NOT EXISTS tags (tag_id unique, tag_name)");
     for(var i = 0; i < alltags.length; i++)
     {
+        console.log("je boucle dans tags");
         var sql = 'INSERT INTO tags (tag_id, tag_name) VALUES (' 
         + alltags[i].tag_id + ', "' + alltags[i].tag_name + '")';
         tx.executeSql(sql);
@@ -167,11 +137,11 @@ function fillTags(tx)
 //remplie la table to_have avec les données du json
 function fillToHave(tx)
 {
-    console.log(allToHave);
-    tx.executeSql("DROP TABLE to_have IF EXISTS");
+    tx.executeSql("DROP TABLE IF EXISTS to_have");
     tx.executeSql("CREATE TABLE IF NOT EXISTS to_have (fk_picture INTEGER, fk_tag INTEGER, FOREIGN KEY(fk_picture) REFERENCES pictures (picture_id), FOREIGN KEY(fk_tag) REFERENCES tags (tag_id))");
     for(var i = 0; i < allToHave.length; i++)
     {
+        console.log("je boucle dans to have");
         var sql = 'INSERT INTO to_have (fk_picture, fk_tag) VALUES (' 
         + allToHave[i].fk_picture + ', ' + allToHave[i].fk_tag + ')';
         tx.executeSql(sql);
--- comVisuelle/platforms/android/app/build/intermediates/assets/debug/www/js/index.js
+++ comVisuelle/platforms/android/app/build/intermediates/assets/debug/www/js/index.js
@@ -21,7 +21,7 @@ function searchAllCategories(tx)
 {
     tx.executeSql('SELECT * FROM categories ORDER BY category_name ASC;', [], function(tx, result){
         categories = result.rows;
-        //console.log(categories);
+        console.log(categories)
         return categories;
     });
 }
@@ -38,39 +38,26 @@ function searchByCategory(tx)
 {
     var catId = parseInt(str);
     numberOfTurn = 0;
-    resetInterface();
-    tx.executeSql('SELECT fk_picture FROM to_belong WHERE fk_category =' + catId, [], function(tx, result)
+
+    tx.executeSql('SELECT fk_picture FROM to_belong WHERE fk_category = ' + catId + ';', [], function(tx, result)
     {
-        console.log("je select la categorie");
         var pic_in_cat = result.rows;
         var picturesFromCategories = [];
         byCat = [];
         for(var i = 0; i < pic_in_cat.length; i++)
         {
-            console.log("je recupere les id des images correspondant a la categorie");
             picturesFromCategories.push(pic_in_cat[i].fk_picture);
         }
         for (var i = 0; i < picturesFromCategories.length; i++)
         {
-            console.log("je coucle dans picturesFromCategories");
-            console.log(picturesFromCategories);
             tx.executeSql('SELECT * FROM pictures WHERE picture_id=' + picturesFromCategories[i] + ';', [],function(tx, picturesFromThisCat){
-                console.log("je select l'image depuis son id");
-                console.log(picturesFromThisCat.rows);
                 byCat.push(picturesFromThisCat.rows[0]);
-                console.log(byCat);
-                console.log(numberOfTurn);
                 if(numberOfTurn == picturesFromCategories.length-1)
                 {
-                    console.log("jai bouclé le dernier");
                     for(var b = 0; b < byCat.length; b++)
                     {
-                        console.log("je push dans byCat les images");
-                        console.log(byCat[b]);
                         byCategory.push(byCat[b]);
-                        console.log(byCategory);
                     }
-                    console.log(byCategory);
                     displaySearchByCategory(byCategory);
                     byCategory = [];
                     byCat = [];
@@ -82,18 +69,11 @@ function searchByCategory(tx)
 
 }
 
-function displayPicturesByCategory()
-{
-    console.log(byCat);
-}
-
 function searchByKeyword(tx)
 {
     var inputVal = $('#inputSearch').val() + "%";
-    //console.log(inputVal);
     tx.executeSql('SELECT * FROM pictures WHERE picture_name LIKE "'+inputVal+'";',[],function(tx,result){
         keyword = result.rows;
-        console.log(keyword);
         displaySearchByKeyword(keyword);
         return keyword;
     });
@@ -103,7 +83,6 @@ function searchByKeyword(tx)
 function displaySearchByKeyword(keyword)
 {
     resetInterface();
-    console.log(keyword);
     for(var k=0; k<keyword.length; k++){
        $('#display').append('<a href="'+keyword[k].picture_url+'" data-lightbox="'+keyword[k].picture_name+'" data-title="'+keyword[k].picture_name+'" class="images"><img src="'+keyword[k].picture_url+'" alt="'+keyword[k].picture_name+'" /></a>');
     }
@@ -112,7 +91,6 @@ function displaySearchByKeyword(keyword)
 function displaySearchByCategory(byCategory)
 {
     resetInterface();
-    console.log(byCategory);
     for(var b = 0; b < byCategory.length; b++){
         $('#display').append('<a href="'+byCategory[b].picture_url+'" data-lightbox="'+byCategory[b].picture_name+'" data-title="'+byCategory[b].picture_name+'" class="images"><img src="'+byCategory[b].picture_url+'" alt="'+byCategory[b].picture_name+'" /></a>');
     }
@@ -122,35 +100,10 @@ function resetInterface()
 {
     $('#display').html("");
 }
-/**
- * Récupérer une image du gestionnaire de fichier de la tablette
- * 
- */
-function cameraGetPicture() {
-    navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
-        destinationType: Camera.DestinationType.FILE_URI,
-        sourceType: Camera.PictureSourceType.SAVEDPHOTOALBUM,
-        targetWidth: 200,
-        targetHeight: 200
-    });
-    
-    function onSuccess(imageURL) {
-        console.log(imageURL)
-        $('#displayImg').append('<img class="imgForm" src="'+imageURL+'">');
-    }
-    
-    function onFail(message) {
-        alert('Failed because: ' + message);
-    }
-    
-}
-
 
 /* =======================================================================================================================
 Event.js handles all click events within the application
 ==========================================================================================================================*/ 
-
-
 
 /* =============  NAVBAR  =========================  NAVBAR ==========================  NAVBAR  ========================*/
 
@@ -179,12 +132,13 @@ $('#admin').on('click', function(){
  */
 
 $('#selectCat').one('click', function(){
-    db.transaction(searchAllCategories, errorCB, successCB);
-    console.log(categories);
-    for(var i=0; i<categories.length; i++){
-        $('#selectCat').append('<option id="'+categories[i].category_id+'" value="'+categories[i].category_id+'">'+categories[i].category_name+'</option>');
-    }
-    return categories;
+    db.transaction(searchAllCategories, console.log("categories inexistante"), function()
+    {
+        for(var i=0; i < categories.length; i++){
+            $('#selectCat').append('<option id="'+categories[i].category_id+'" value="'+categories[i].category_id+'">'+categories[i].category_name+'</option>');
+        }
+        return categories;
+    });
 });
 
 /***
@@ -193,7 +147,6 @@ $('#selectCat').one('click', function(){
 
 $('#selectCat').change(function(){
     str = $(this).children(":selected").attr("id");
-    console.log("j'ai changé");
     db.transaction(searchByCategory, errorCB, successCB);
 });
 
@@ -212,49 +165,4 @@ $('#inputSearch').on('click', function(){
 
 $('#inputSearch').keyup(function(){
     db.transaction(searchByKeyword, errorCB, successCB);
-    console.log(keyword);
 });
-
-
-
-
-/* =========================  FORMULAIRE  =======================  FORMULAIRE ======================================= */
-
-/***
- * Button Choose a file Form
- */
-
-$('#inputFile').on('click', cameraGetPicture,function(){
-    window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function (dirEntry) {
-        var isAppend = true;
-        createFile(dirEntry, "fileToAppend.txt", isAppend);
-    }, errorHandler);
-});
-
-function errorHandler(e) {
-    var msg = '';
-    switch (e.code) {
-        case FileError.QUOTA_EXCEEDED_ERR:
-            msg = 'QUOTA_EXCEEDED_ERR';
-            break;
-        case FileError.NOT_FOUND_ERR:
-            msg = 'NOT_FOUND_ERR';
-            break;
-        case FileError.SECURITY_ERR:
-            msg = 'SECURITY_ERR';
-            break;
-        case FileError.INVALID_MODIFICATION_ERR:
-            msg = 'INVALID_MODIFICATION_ERR';
-            break;
-        case FileError.INVALID_STATE_ERR:
-            msg = 'INVALID_STATE_ERR';
-            break;
-        default:
-            msg = 'Unknown Error';
-            break;
-    };
-    console.log('Error: ' + msg);
-}
-/***
- * Button Confirm Form
- */
